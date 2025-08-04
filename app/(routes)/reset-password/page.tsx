d367adbca3afd17468e88cbb8e5c71d64e248932
mario ruby <mariogabrielrn@gmail.com>
2025-08-04 14:22:47 +0200

    promesss

--- app/(routes)/reset-password/page.tsx
+++ app/(routes)/reset-password/page.tsx
@@ -1,11 +1,13 @@
-import ResetPasswordForm from "@/components/reset-password"
+import ResetPasswordForm from "@/components/reset-password";
+// import { type SearchParams } from "next";
 
 interface Props {
-  searchParams: { [key: string]: string | string[] | undefined }
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
-export default function ResetPassword({ searchParams }: Props) {
-  const token = typeof searchParams.token === "string" ? searchParams.token : ""
+export default async function ResetPassword({ searchParams }: Props) {
+  const params = await searchParams; // Resuelve la Promise
+  const token = typeof params.token === "string" ? params.token : "";
 
   return (
     <div className="bg-background flex min-h-screen flex-col items-center justify-center gap-6 p-6 md:p-10">
@@ -13,5 +15,5 @@ export default function ResetPassword({ searchParams }: Props) {
         <ResetPasswordForm token={token} />
       </div>
     </div>
-  )
+  );
 }
